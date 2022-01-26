07678623f8ae2a3dd559e4cef11fb424f02d4463
G-wei-bd <2604325988@qq.com>
2022-01-26 09:42:22 +0800

    2022-01-26

--- pages/practicePage/practice/practice.vue
+++ pages/practicePage/practice/practice.vue
@@ -1,12 +1,22 @@
 <template>
 	<view>
-		<view class="">
+		<view class="practiceContainer">
 			<uni-card title="实习情况" extra="额外信息">
 				<uni-list>
-					<uni-list-item title="列表文字1" note=""></uni-list-item>
-					<uni-list-item title="列表文字2" note=""></uni-list-item>
-					<uni-list-item title="列表文字3" note=""></uni-list-item>
-					<uni-list-item title="列表文字4" note=""></uni-list-item>
+					<uni-list-item title="实习计划" rightText="18网络工程 生产实习"></uni-list-item>
+					<uni-list-item title="实践课程" rightText="生产实习"></uni-list-item>
+					<uni-list-item title="实践周数" rightText="20周"></uni-list-item>
+					<uni-list-item title="参与实习时间" rightText="2021-09-01~2022-01-28"></uni-list-item>
+					<uni-list-item title="参与状态">
+						<text class="text-danger text-right small" slot="footer">已参与</text>
+					</uni-list-item>
+					<uni-list-item title="实习岗位" rightText="前端开发"></uni-list-item>
+					<uni-list-item title="实习指导老师" rightText="不知道"></uni-list-item>
+					<uni-list-item title="实习进展">
+						<view class="text-warning" slot="footer">
+							<uni-tag text="三方协议" size="small" type="primary"></uni-tag>	
+						</view>
+					</uni-list-item>
 				</uni-list>
 			</uni-card>
 		</view>
