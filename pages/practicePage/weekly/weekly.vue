52dddec7fa69443ccdd8765d93d4a08f48e72b42
G-wei-bd <2604325988@qq.com>
2022-01-29 17:34:03 +0800

    2022.01.29·17.33

--- pages/practicePage/weekly/weekly.vue
+++ pages/practicePage/weekly/weekly.vue
@@ -1,8 +1,21 @@
 <template>
 	<view>
 		<view class="weeklyContainer">
-			<uni-card title="标题文字" extra="额外信息">
-				显示周记篇数的卡片栏
+			<uni-card title="周记详情">
+				<uni-table stripe emptyText="暂无更多数据">
+					<uni-tr>
+						<uni-th align="center">参与项目</uni-th>
+						<uni-th align="center">实习岗位</uni-th>
+						<uni-th align="center">实习时间</uni-th>
+						<uni-th align="center">周记篇数</uni-th>
+					</uni-tr>
+					<uni-tr>
+						<uni-td align="center">18网络工程实习</uni-td>
+						<uni-td align="center">前端开发</uni-td>
+						<uni-td align="center">2021.09.01 ~ 2022.01.31</uni-td>
+						<uni-td align="center">20篇</uni-td>
+					</uni-tr>
+				</uni-table>
 			</uni-card>
 		</view>
 		<view>
@@ -97,7 +110,9 @@
 			// 分页触发
 			change(e) {
 				console.log(e);
-				console.log(this.$refs.table)
+				console.log(this.$refs.table.clearSelection());
+				this.$refs.table.clearSelection();
+				this.selectedIndexs.length = 0;
 				
 			}
 			
