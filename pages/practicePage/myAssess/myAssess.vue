f78632445183d255e8a59ce73c0b6dca29051168
G-wei-bd <2604325988@qq.com>
2022-02-03 19:47:56 +0800

    2022.02.03·19.48

--- pages/practicePage/myAssess/myAssess.vue
+++ pages/practicePage/myAssess/myAssess.vue
@@ -1,19 +1,22 @@
 <template>
 	<view>
 		<text>这是我的评价的页面，负责编写我的评价</text>
+		<uni-rate v-model="value" allowHalf @change="onChange"/>
 	</view>
 </template>
 
 <script>
 	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
+	    data() {
+	        return {
+	            value: 2
+	        }
+	    },
+	    methods: {
+	        onChange(e) {
+	            console.log('rate发生改变:' + JSON.stringify(e))
+	        }
+	    }
 	}
 </script>
 
