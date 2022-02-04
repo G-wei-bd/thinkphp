617713e5e02deb66c3fb8ffc64cac98aca92a631
G-wei-bd <2604325988@qq.com>
2022-02-04 17:31:41 +0800

    2022.02.04·17.31

--- pages/practicePage/myAssess/myAssess.vue
+++ pages/practicePage/myAssess/myAssess.vue
@@ -1,25 +1,47 @@
 <template>
 	<view>
-		<text>这是我的评价的页面，负责编写我的评价</text>
-		<uni-rate v-model="value" allowHalf @change="onChange"/>
+		<view class="assessContainer">
+			<view class="cardContainer">
+				<uni-card title="我的实习评价">
+						<uni-list>
+							<uni-list-item title="参与计划" rightText="18实习"></uni-list-item>
+							<uni-list-item title="参与时间" rightText="2021.09.01 ~ 2022.01.31"></uni-list-item>
+							<uni-list-item title="指导老师" rightText="不知道"></uni-list-item>
+							<uni-list-item title="是否评价" note="未评价">
+								<view slot="footer">
+									<button class="btn btn-danger btn-sm">添加评价</button>
+								</view>
+							</uni-list-item>
+						</uni-list>
+				</uni-card>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
-	    data() {
-	        return {
-	            value: 3
-	        }
-	    },
-	    methods: {
-	        onChange(e) {
-	            console.log('rate发生改变:' + JSON.stringify(e))
-	        }
-	    }
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
 	}
 </script>
 
 <style>
-
+	.assessContainer{
+		display: flex;
+	}
+	.cardContainer{
+		width: 100vw;
+	}
+	.rightContainer{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
