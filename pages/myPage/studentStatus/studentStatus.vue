a98211732cadb9c9fe7b3ce61b4cae60f8173ff8
G-wei-bd <2604325988@qq.com>
2022-02-09 11:45:13 +0800

    2022.02.09·11.45

--- pages/myPage/studentStatus/studentStatus.vue
+++ pages/myPage/studentStatus/studentStatus.vue
@@ -4,7 +4,7 @@
 			<button class="btn btn-danger btn-outline-danger" @click="openPopup">+ 添加学籍</button>
 			<uni-popup ref="popup" type="center">
 				<view class="addInfo">
-					<uni-forms ref="form" :modelValue="formData">
+					<uni-forms ref="form" :modelValue="formData" :rules="rules">
 						<uni-forms-item required label="学习时间" name="time">
 							<uni-datetime-picker v-model="formData.time" type="daterange" :clearIcon="false"
 								start="1990-01-01" end="2099-12-31" rangeSeparator="~" />
@@ -13,16 +13,16 @@
 							<uni-datetime-picker type="date" :clearIcon="false" v-model="formData.intoTime" />
 						</uni-forms-item>
 						<uni-forms-item required label="学校" name="university">
-							<uni-easyinput v-model="formData.university" placeholder="请输入学校名称" />
+							<uni-easyinput v-model="formData.university" trim="all" placeholder="请输入学校名称" />
 						</uni-forms-item>
 						<uni-forms-item required label="院系" name="collage">
-							<uni-easyinput v-model="formData.collage" placeholder="请输入院系名称" />
+							<uni-easyinput v-model="formData.collage" trim="all" placeholder="请输入院系名称" />
 						</uni-forms-item>
 						<uni-forms-item required label="专业" name="major">
-							<uni-easyinput v-model="formData.major" placeholder="请输入专业名称" />
+							<uni-easyinput v-model="formData.major" trim="all" placeholder="请输入专业名称" />
 						</uni-forms-item>
 						<uni-forms-item required label="班级" name="majorClass">
-							<uni-easyinput v-model="formData.majorClass" placeholder="请输入班级" />
+							<uni-easyinput v-model="formData.majorClass" trim="all" placeholder="请输入班级" />
 						</uni-forms-item>
 					</uni-forms>
 					<view class="submitBtn">
@@ -41,10 +41,10 @@
 						<uni-th align="center">院系</uni-th>
 						<uni-th align="center">专业</uni-th>
 						<uni-th align="center">班级</uni-th>
-						<uni-th align="center">入学年份</uni-th>
+						<uni-th align="center">入学时间</uni-th>
 						<uni-th align="center">审核状态</uni-th>
 					</uni-tr>
-					<uni-tr v-for="(item,index) in Arr" :key="index">
+					<uni-tr v-for="(item, index) in Arr" :key="index">
 						<uni-td align="center">{{index + 1}}</uni-td>
 						<uni-td align="center">{{item.time[0]}} ~ {{item.time[1]}}</uni-td>
 						<uni-td align="center">{{item.university}}</uni-td>
@@ -67,12 +67,51 @@
 		data() {
 			return {
 				formData: {
-					time: ["2018-09-1", "2022-6-30"],
+					time: '',
 					university: '',
 					collage: '',
 					major: '',
 					majorClass: '',
-					intoTime: '2022-02-01'
+					intoTime: ''
+				},
+				rules: {
+					time: {
+						rules: [{
+							required: true,
+							errorMessage: "请选择学习时间"
+						}, ]
+					},
+					intoTime: {
+						rules: [{
+							required: true,
+							errorMessage: "请选择入学时间"
+						}, ]
+					},
+					university: {
+						rules: [{
+							required: true,
+							errorMessage: "请填写学校"
+						}, ]
+					},
+					collage: {
+						rules: [{
+							required: true,
+							errorMessage: "请填写院系"
+						}, ]
+					},
+					major: {
+						rules: [{
+							required: true,
+							errorMessage: "请填写专业"
+						}, ]
+					},
+					majorClass: {
+						rules: [{
+							required: true,
+							errorMessage: "请填写所在班级"
+						}, ]
+					}
+
 				},
 				Arr: [{
 					time: ["2017-09-1", "2021-6-30"],
@@ -97,6 +136,7 @@
 					this.formData.time = res.time;
 					this.formData.intoTime = res.intoTime;
 					this.Arr.push(this.formData);
+					this.formData = {};
 					this.$refs.popup.close();
 				}).catch((err) => {
 					console.log(err)
